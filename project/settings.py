101414fcd1cd58b7671810075dde1139037b6e7e
Nikolay Vereshchagin <nick.vereshchagin@gmail.com>
2021-10-27 16:17:19 +0300

    prepare for deploy

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    '192.168.1.49'
+    '192.168.1.49',
+    'http://nikololoshka.pythonanywhere.com/'
 ]
 
 # Application definition
@@ -146,6 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- project/urls.py
+++ project/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', lambda request: redirect('schedule-api-root')),
     path('admin/', admin.site.urls),
     path('stankinschedule/api/', include('apps.stankin_schedule_api.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
